9f4012a4a88874d0150d42d725a891f6df4fb886
sakitha <kukansakitha@gmail.com>
2022-07-07 12:29:49 +0530

    Updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,5 @@
-const noteRoutes = require('./note_routes');module.exports = function(app, db) {
-    noteRoutes(app, db);
-    // Other route groups could go here, in the future
-  };
+const noteRoutes = require("./note_routes");
+module.exports = function (app, db) {
+  noteRoutes(app, db);
+  // Other route groups could go here, in the future
+};
